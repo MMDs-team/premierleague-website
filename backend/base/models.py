808da546f980bc100fde7d644b79d2a1b0b5bc65
mmd <mohmad1382m@gmail.com>
2024-06-13 16:49:37 +0330

    + added group 3 of models + completed ActionType model Day #1 (test)

--- backend/base/models.py
+++ backend/base/models.py
@@ -5,3 +5,61 @@ SHORT_STRLEN = 10
 SMALL_STRLEN = 25
 STRLEN = 45
 LONG_STRLEN = 125
+
+class ActionType(models.Model) : 
+    action_type_id = models.IntegerField(primary_key=True, unique=True)
+    subtype = models.CharField(max_length=SMALL_STRLEN, blank=False, null=False)
+    type = models.CharField(max_length=SMALL_STRLEN, blank=False, null=False)
+    
+
+class Action(models.Model) : 
+    '''
+    action_id = 
+    match_id =
+    action_type_id = 
+    subject_id = 
+    object_id = 
+    subject_from_home = 
+    time = 
+    '''
+
+class Player(models.Model) : 
+    '''
+    player_id = 
+    image_path = 
+    birth_date = 
+    nationality = 
+    height = 
+    social_media = 
+    heath_state
+    '''
+
+class SamplePlayer(models.Model) : 
+    '''
+    player_id = 
+    club_id = 
+    season_id = 
+    number_in_team = 
+    position = 
+    '''
+
+class Transfer(models.Model) : 
+    '''
+    player_id = 
+    season_id = 
+    origin_club_id =
+    target_club_id =
+    date =
+    amount =
+    semester = 
+    duration = 
+    '''
+
+class Refree(models.Model) : 
+    '''
+    refree_id =
+    image_path = 
+    birth_date =
+    cv_path = 
+    nationality =
+    '''

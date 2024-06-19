e7b946a23dba41d663c81b5840f5a1d000bbc6f7
mmd <mohmad1382m@gmail.com>
2024-06-19 20:27:05 +0330

    + some errors were fixed

--- backend/base/models.py
+++ backend/base/models.py
@@ -3,7 +3,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 
-SEMESTER = [('SUMMER', 'summer'), ('WINTER', 'winter')]
+SEMESTERS = [('SUMMER', 'summer'), ('WINTER', 'winter')]
 SHORT_STRLEN = 10
 SMALL_STRLEN = 25
 STRLEN = 45
@@ -16,8 +16,43 @@ class ActionType(models.Model) :
     
     def __str__(self):
         return f'{self.type}-{self.subtype}'
-    
 
+class Refree(models.Model) : 
+    refree = models.OneToOneField(User, primary_key=True, unique=True, editable=False, on_delete=models.CASCADE)
+    
+    image = models.ImageField(upload_to='refrees', max_length=STRLEN, null=True, blank=True)
+    birth_date = models.DateField()
+    cv = models.FileField(upload_to='CVs')
+    nationality = models.CharField(max_length=SMALL_STRLEN)
+    
+    def __str__(self):
+        return f'{self.refree}'
+    
+class Player(models.Model) : 
+    player = models.OneToOneField(User, primary_key=True, auto_Increment=True, on_delete=models.CASCADE)
+    
+    image = models.ImageField(upload_to='players', max_length=STRLEN, null=True, blank=True)
+    birth_date = models.DateField()
+    nationality = models.CharField(max_length=SMALL_STRLEN)
+    height = models.IntegerField()
+    social_media = models.CharField(max_length=STRLEN, null=True, default='NULL')
+    position = models.CharField(max_length=SMALL_STRLEN)
+    heath_state = models.BooleanField(default=True)
+    
+    seasons = models.ManyToManyField(
+        Season, 
+        through='Transfer', 
+        through_fields=('player', 'season')
+    )   
+    clubs = models.ManyToManyField(
+        Club, 
+        through='Transfer', 
+        through_fields=('player', 'target_club', 'origin_club')
+    )
+        
+    def __str__(self):
+        return f'{self.player_id}-{self.position}'
+    
 class Action(models.Model) : 
     action_type = models.ForeignKey(ActionType, on_delete=models.CASCADE)
     match = models.ForeignKey(Match, on_delete=models.CASCADE)
@@ -31,35 +66,9 @@ class Action(models.Model) :
     def __str__(self):
         return f'{self.action_id}-{self.time}'
 
-class Player(models.Model) : 
-    player = models.OneToOneField(User, primary_key=True, auto_Increment=True, on_delete=models.CASCADE)
-    
-    image = models.ImageField(upload_to='player', max_length=STRLEN, null=True, blank=True)
-    birth_date = models.DateField()
-    nationality = models.CharField(max_length=SMALL_STRLEN)
-    height = models.IntegerField()
-    social_media = models.CharField(max_length=STRLEN, null=True, default='NULL')
-    position = models.CharField(max_length=SMALL_STRLEN) # add
-    heath_state = models.BooleanField(default=True)
-    
-    seasons = models.ManyToManyField(
-        Season, 
-        through='Transfer', 
-        through_field=('player', 'season')
-    )   
-    clubs = models.ManyToManyField(
-        Club, 
-        through='Transfer', 
-        through_field=('player', 'target_club', 'origin_club')
-    )
-        
-    def __str__(self):
-        return f'{self.player_id}-{self.position}'
-
 class SamplePlayer(models.Model) : 
     player = models.ForeignKey(Player, primary_key=True, on_delete=models.CASCADE)
     club = models.ForeignKey(SampleClub, primary_key=True, on_delete=models.CASCADE)
-    # season = models.ForeignKey(SampleClub, on_delete=models.CASCADE)    
     
     number_in_team = models.SmallIntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])
     position = models.CharField(max_length=SMALL_STRLEN)
@@ -81,16 +90,6 @@ class Transfer(models.Model) :
     def __str__(self):
         return f'{self.player}: {self.origin_club} -> {self.target_club}, {self.season}-{self.semester}'
 
-class Refree(models.Model) : 
-    refree = models.OneToOneField(User, primary_key=True, unique=True, editable=False, on_delete=models.CASCADE)
-    
-    image = models.ImageField(upload_to='refree', max_length=STRLEN, null=True, blank=True)
-    birth_date = models.DateField()
-    cv = models.FileField(upload_to='CVs')
-    nationality = models.CharField(max_length=SMALL_STRLEN)
-    
-    def __str__(self):
-        return f'{self.refree}'
     
 class Match(models.Model) : 
     match_id = models.AutoField(primary_key=True, unique=True, editable=False)
@@ -103,7 +102,6 @@ class Match(models.Model) :
     
     host_club = models.ForeignKey(SampleClub, on_delete=models.CASCADE)
     guest_club = models.ForeignKey(SampleClub, on_delete=models.CASCADE)
-    # season = models.ForeignKey(SampleClub, on_delete=models.CASCADE)    
     refree = models.ForeignKey(Refree, on_delete=models.CASCADE)
     first_refree_asist = models.ForeignKey(Refree, on_delete=models.CASCADE)
     second_refree_asist = models.ForeignKey(Refree, on_delete=models.CASCADE)
@@ -112,12 +110,12 @@ class Match(models.Model) :
     match_spons = models.ManyToManyField(
         Sponsor, 
         through='MatchSpon', 
-        through_field=('match', 'player')
+        through_fields=('match', 'player')
     )   
     casts = models.ManyToManyField(
         Broadcaster, 
         through='Casts', 
-        through_field=('match', 'broadcaster')
+        through_fields=('match', 'broadcaster')
     )
     
     def __str__(self):

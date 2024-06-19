2d42a502cc20b7d738f6ef6c64f7aaacdeb92f83
Mohammad Khandan <mdKhandan28@gmail.com>
2024-06-19 14:02:38 +0330

    Adding relations between classes in "models.py"!

--- backend/base/models.py
+++ backend/base/models.py
@@ -1,28 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
 SHORT_STRLEN = 10
 SMALL_STRLEN = 25
 STRLEN = 45
 LONG_STRLEN = 125
-
-"""
-    TODO:
-    -> Add keys.
-"""
-
-"""
-group1: -> Khandan
-Club
-SampleClub
-kit
-Sponser
-ClubSpon
-MatchSpon
-SeasonSpon
-BroadCaster
-Casts
-"""
 
 class Club(models.Model):
     club_id = models.AutoField(primary_key=True, editable=False)
@@ -34,16 +17,38 @@ class Club(models.Model):
     social_media = models.URLField(max_length=LONG_STRLEN)
     email = models.Email.Field(max_length=LONG_STRLEN)
 
+    sponsors = models.ManyToMany(
+        "Sponsor",
+        through="ClubSpon",
+        through_fields=("club", "sponsor")
+    )
+    
+    stadiums = models.ManyToMany(
+        Stadium,
+        through="ClubStad",
+        through_fields=("club", "stadium")
+    )
+
+
+class ClubStad(models.Model):
+    club = models.ForeignKey(Club, on_delete=models.CASCADE)
+    stadium = models.ForeignKey(Stadium, models.CASCADE)
+
+    # Discuss about how to add a delimiter related with season here!
+
 
 class SampleClub(models.Model):
     logo = models.ImageField(upload_to="sample_clubs")
     total_points = models.PositiveIntegerField()
 
+    club = models.ForeignKey(Club, on_delete=models.CASCADE)
+    season = modesl.ForeignKey(Season, on_delete=models.CASCADE)
 
-class Kit(models.Model):
-    kit_id = models.AutoField(primary_key=True, editable=False)
-    image = models.ImageField(upload_to="kits")
-    color = models.CharField(max_length=SHORT_STRLEN)
+    players = models.ManyToMany(
+        Player,
+        through="SamplePlayer",
+        through_field=("sample_player", "player")
+    )
 
 
 class Sponsor(models.Model):
@@ -54,18 +59,33 @@ class Sponsor(models.Model):
 
 
 class ClubSpon(models.Model):
-    ...
+    club = models.ForeignKey(Club, on_delete=models.CASCADE)
+    sponsor = models.ForeignKey(Sponsor, on_delete=models.CASCADE)
+    
+
+class Kit(models.Model):
+    kit_id = models.AutoField(primary_key=True, editable=False)
+    image = models.ImageField(upload_to="kits")
+    color = models.CharField(max_length=SHORT_STRLEN)
+
+    sample_club = models.ForeignKey(SampleClub, on_delete=models.CASCADE)
 
 
 class MatchSpon(models.Model):
     amount = models.PositiveIntegerField()
     gif = models.FileField(upload_to="match_sponsors")
 
+    _match = models.ForeignKey(Match, on_delete=models.CASCADE)
+    sponsor = models.ForeignKey(Sponsor, on_delete=models.CASCADE)
 
-class SeasonSpon(models.Model):
+
+class SeaSpon(models.Model):
     image = models.ImageField(upload_to="season_sponsors")
     amount = models.PositiveIntegerField()
     link = models.URLField(max_length=LONG_STRLEN)
+
+    sponsor = models.ForeignKey(Sponsor, on_delete=models.CASCADE)
+    season = models.ForeignKey(Season, on_delete=models.CASCADE)
 
 
 class Broadcaster(models.Model):
@@ -77,4 +97,5 @@ class Broadcaster(models.Model):
 
 
 class Casts(models.Model):
-    ...
+    _match = models.ForeignKey(Match, on_delete=models.CASCADE)
+    broadcaster = models.ForeignKey(Broadcaster, on_delete=models.CASCADE)

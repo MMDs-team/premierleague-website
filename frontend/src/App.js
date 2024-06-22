fd58a08399560fa3a153aca02ed9f63f73e47674
mohammad-ghaderi <mohammadunique2002@gmail.com>
2024-06-22 19:08:27 +0330

    Some of routes was added
    Day #6

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,33 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate, useNavigate } from "react-router-dom";
+import LoginScreen from './screens/LoginScreen.js';
+import RegisterScreen from './screens/RegisterScreen.js';
+import HomeScreen from './screens/HomeScreen.js';
+import ProfileScreen from './screens/ProfileScreen.js';
 
-function App() {
+
+function NoPage() {
+    const navigate = useNavigate();
+    navigate('/');
+    return <Navigate to={ '/'} />;
+}
+
+
+export default function App() {
+
   return (
-    <div className="App">
-      Premier league
-    </div>
+    <BrowserRouter>
+        <Routes>
+            <Route path="*" element={<NoPage/>} />
+            <Route path="/login" element={ <LoginScreen/>} />
+            <Route path="/register" element={<RegisterScreen/>} />
+            <Route path="/profile" element={<ProfileScreen/>} />
+            <Route exact path="/" element={<HomeScreen />}>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
-export default App;
+
+

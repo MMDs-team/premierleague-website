d5a2a5c555e4a15c15914e7032cdebe2f94fd8e3
mohammad-ghaderi <mohammadunique2002@gmail.com>
2024-06-25 22:10:19 +0330

    the view for create club added
    Day #9

--- backend/base/views/club_views.py
+++ backend/base/views/club_views.py
@@ -2,6 +2,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from rest_framework import status
+import datetime
 
 from base.models import Club
 from base.serializers.club_serializers import *
@@ -17,9 +18,42 @@ def get_all_clubs(request):
 @api_view(['GET'])
 def get_single_club(request, pk):
     try:
-        club = Club.objects.get(club_id = pk)
+        club = Club.objects.get(pk = pk)
         serializer = ClubSerializer(club, many=False)
         return Response(serializer.data)
     except :
-        return Response({'detail': 'No Club found with this id'}, status=status.HTTP_404_NOT_FOUND)
+        message = {'detail': 'No Club found with this id'}
+        return Response(message, status=status.HTTP_404_NOT_FOUND)
         
+
+@api_view(['POST'])
+def add_club(request):
+    data = request.data
+    # "1956-10-19"
+    y = int(data['est_date'][:4])
+    m = int(data['est_date'][5:7])
+    d = int(data['est_date'][8:])
+    print("year=", y)
+    print("mounth=", m)
+    print("day=", d)
+    date = datetime.date(y, m, d)
+
+    try:
+        club = Club.objects.create(
+            name = data['name'],
+            image = data['image'],
+            description = data['description'],
+            est_date = date,
+            website = data['website'],
+            social_media = data['social_media'],
+            email = data['email']
+        )
+
+        serializer = ClubSerializer(club, many=False)
+
+        return Response(serializer.data)
+    except :
+        message = {'detail': 'Error while creating club'}
+        return Response(message, status=status.HTTP_400_BAD_REQUEST)
+
+

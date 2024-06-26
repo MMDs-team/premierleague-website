b149c3f763c380e1d74a0a13bcb727b6f4657bc1
mohammad-ghaderi <mohammadunique2002@gmail.com>
2024-06-26 13:06:53 +0330

    The sample player updated
    Day #10

--- backend/base/urls/player_urls.py
+++ backend/base/urls/player_urls.py
@@ -10,3 +10,13 @@ urlpatterns = [
     path('', views.get_all_players, name='all_players'),
 
 ]
+
+
+urlpatterns = [
+    path('samplePlayer/add', views.add_sample_club, name='add_sample_club'),
+    path('samplePlayer/update/<int:pk>', views.update_sample_club, name='update_sample_club'),
+    path('samplePlayer/remove/<int:pk>', views.remove_sample_club, name='remove_sample_club'),
+    path('samplePlayer/<int:pk>', views.get_single_sample_club, name='get_single_sample_club'),
+    path('samplePlayer', views.get_all_sample_clubs, name='all_sample_clubs'),
+
+] + urlpatterns

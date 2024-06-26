5a13f036a1fd216b22b561b4f9896f4519835262
Mohammad Khandan <mdKhandan28@gmail.com>
2024-06-26 18:42:16 +0330

    Serializer of employees have been changed!

--- backend/base/serializers/employee_serializers.py
+++ backend/base/serializers/employee_serializers.py
@@ -1,10 +1,15 @@
 from rest_framework import serializers
 from base.models import Employee
+from base.serializers.user_serializers import SimpleUserSerializer
 
 class EmployeeSerializer(serializers.ModelSerializer):
     employee = serializers.SerializerMethodField(read_only=True)
 
-    def get_employee(self, obj): return obj.employee
+    def get_employee(self, obj):
+        user = obj.employee
+        serializer = SimpleUserSerializer(user, many=False)
+        
+        return serializer.data
 
     class Meta:
         model = Employee

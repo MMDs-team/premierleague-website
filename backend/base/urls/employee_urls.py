65810c12831885d1fc1d14fb3584d2f89dbbda9c
Mohammad Khandan <mdKhandan28@gmail.com>
2024-06-26 04:42:03 +0330

    Little bug fix.

--- backend/base/urls/employee_urls.py
+++ backend/base/urls/employee_urls.py
@@ -3,7 +3,7 @@ from base.views import employee_views as views
 
 
 urlpatterns = [
-    #     path('add', views.add_employee, name='add_employee'),
+    path('add', views.add_employee, name='add_employee'),
     path('update/<int:pk>', views.update_employee, name='update_employee'),
     path('remove/<int:pk>', views.remove_employee, name='remove_employee'),
     path('<int:pk>', views.get_single_employee, name='get_single_employee'),

90671297872d42c00d76aad385269a001d0d8ac6
mohammad-ghaderi <mohammadunique2002@gmail.com>
2024-06-28 11:06:03 +0330

    fetchin data from backend
    Day #12

--- frontend/src/components/ClubNavigation.jsx
+++ frontend/src/components/ClubNavigation.jsx
@@ -1,16 +1,26 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
 import clubLogo from '../static/images/clubImage.png'
 
 export const ClubNavigation = () => {
 
+    const [clubs, setClubs] = useState([])
 
-    let clubs = [
-        0,0,0,0,0,
-        0,0,0,0,0,
-        0,0,0,0,0,
-        0,0,0,0,0,
-    ]
+    const fetchData = async () => {
+        try {
+            const response = await axios.get("http://127.0.0.1:8000/api/club/ex")
+            setClubs(response.data)
+            console.log(response.data)
+        } catch (error) {
+            console.log("Error fetching clubs!", error)
+        }
+
+        
+    }
+
+    useEffect(() => {
+        fetchData();
+    },[])
 
 
     return (
@@ -23,12 +33,12 @@ export const ClubNavigation = () => {
                 {clubs.map((club, i) => (
                     
                     <li className="clubList__club" key={i}>
-                        <a className="clubList__link" target="_blank" href="/" role="menuitem">
+                        <a className="clubList__link" target="_blank" href={club.website} role="menuitem">
                             <div className="badge badge--large badge-image-container" data-widget="club-badge-image" data-size="50">
-                                <img className="badge-image badge-image--50 js-badge-image" src={clubLogo} alt='club_logo'/>
-                                <span className="visuallyHidden">Arsenal</span>
+                                <img className="badge-image badge-image--50 js-badge-image" src={`http://127.0.0.1:8000/${club.logo}`} alt='club_logo'/>
+                                <span className="visuallyHidden">{club.name}</span>
                             </div>
-                            <span className="name">Arsenal</span>
+                            <span className="name">{club.name}</span>
                         </a>
                     </li>
 

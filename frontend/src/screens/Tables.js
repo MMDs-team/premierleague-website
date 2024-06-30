9d12a1543f197abc3f181a7ff5053dd485f71811
mohammad-ghaderi <mohammadunique2002@gmail.com>
2024-07-01 00:20:09 +0330

    Adding authurzaition
    Day #15

--- frontend/src/screens/Tables.js
+++ frontend/src/screens/Tables.js
@@ -1,9 +1,38 @@
+import axios from 'axios';
 import React from 'react'
 
 const Tables = () => {
+  // a local state to store the currently selected file.
+  const [selectedFile, setSelectedFile] = React.useState(null);
+
+  const handleSubmit = async(event) => {
+    event.preventDefault()
+    const formData = new FormData();
+    formData.append("selectedFile", selectedFile);
+    formData.append("selectedFile", selectedFile);
+    formData.append("selectedFile", selectedFile);
+    try {
+      const response = await axios({
+        method: "post",
+        url: "http://127.0.0.1:8000/api/user/say",
+        data: formData,
+        headers: { "Content-Type": "multipart/form-data" },
+      });
+    } catch(error) {
+      console.log(error)
+    }
+  }
+
+  const handleFileSelect = (event) => {
+    setSelectedFile(event.target.files[0])
+  }
+
   return (
-    <div>Tables</div>
+    <form onSubmit={handleSubmit}>
+      <input type="file" onChange={handleFileSelect}/>
+      <input type="submit" value="Upload File" />
+    </form>
   )
-}
+};
 
 export default Tables

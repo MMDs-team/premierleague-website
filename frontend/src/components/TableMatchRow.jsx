661d7bda6be44fd9ede383abaf7cd4bbe5eef65f
mohammad-ghaderi <mohammadunique2002@gmail.com>
2024-07-01 18:04:39 +0330

    Table screen and fetching data
    Day #15

---
+++ frontend/src/components/TableMatchRow.jsx
@@ -0,0 +1,79 @@
+import React from 'react'
+
+export const TableMatchRow = (props) => {
+
+    const {club_id, name, logo, played, won, drawn, lost, goal, goal_conCeded, goal_difference, point, last_matches} = props.data
+    const {idx} = props
+
+    return (
+
+        <tr className="tableDark" data-compseason="578" data-position="1" data-filtered-entry-size="20" data-filtered-table-row="11" data-filtered-table-row-name="Manchester City" data-filtered-table-row-abbr="MCI">
+            <td className="league-table__pos pos button-tooltip" tabindex="0" id="Tooltip">
+                <span className="league-table__value value">{idx+1} </span>
+                <span className="league-table__movement none">
+                    <span className="league-table__tooltip-container tooltipContainer tooltip-left" role="tooltip">
+                        <span className="league-table__tooltip-content tooltip-content">Previous Position
+                            <span className="league-table__result-highlight">
+                            </span>
+                        </span>
+                    </span>
+                </span>
+            </td>
+            <td className="league-table__team team" scope="row">
+                <a href="//www.premierleague.com/clubs/11/Manchester-City/overview">
+                    <span className="badge badge-image-container" data-widget="club-badge-image" data-size="50">
+                        <img className="badge-image badge-image--32 js-badge-image" src={`http://127.0.0.1:8000${logo}`}  />
+                    </span>
+                    <span className="league-table__team-name league-table__team-name--long long">{name}</span>
+                    <span className="league-table__team-name league-table__team-name--short short">{name.slice(0,3).toUpperCase()}</span>       {/*the short form for club -------------------------------------------------------*/}
+                </a>
+            </td>
+            <td>{played}</td> 
+            <td>{won}</td>
+            <td>{drawn} </td>
+            <td>{lost}</td>
+            <td className="hideSmall">{goal}</td>
+            <td className="hideSmall">{goal_conCeded} </td>
+            <td>{goal_difference} </td>
+            <td className="league-table__points points">{point}</td>
+            <td className="league-table__form form hideMed">
+                <ul>    {console.log("last_matches", last_matches)}
+                    {   
+                        last_matches.length > 0 && last_matches.map((match, index) => (
+
+                            <li tabindex="0" className={match[0]>match[3] ? 'win button-tooltip' : match[0] < match[3] ? 'lose button-tooltip' : 'draw button-tooltip'} id="Tooltip" key={index}>
+                                <abbr title="Won" className="form-abbreviation">{match[0]>match[3] ? 'W' : match[0] < match[3] ? 'L' : 'D'}
+                                </abbr>
+                                <a href="//www.premierleague.com/match/93667" className="tooltipContainer linkable tooltip-link tooltip-right" role="tooltip">
+                                    <span className="tooltip-content">
+                                        <div className="match-fixture match-fixture--small">
+                                            <span className="match-fixture__match-info matchInfo"> {/* the date need----------------------------------------------------------------< */}
+                                            </span>
+                                            <div className="match-fixture__team-score-container">
+                                                <div className="match-fixture__team match-fixture__team--home">
+                                                    <span className="atch-fixture__team-name"> {match[1].slice(0,3).toUpperCase()} </span>    {/*the short form for club -------------------------------------------------------*/}
+                                                    <span className="badge badge--home-team badge-image-container" data-widget="club-badge-image" data-size="50">
+                                                        <img className="badge-image badge-image--30 js-badge-image" src={`http://127.0.0.1:8000${match[2]}`} />
+                                                    </span>
+                                                </div>
+                                                <span className="match-fixture__score score"> {match[6]} </span>
+                                                <div className="match-fixture__team match-fixture__team--home">
+                                                    <span className="badge badge--away-team badge-image-container" data-widget="club-badge-image" data-size="50">
+                                                        <img className="badge-image badge-image--30 js-badge-image" src={`http://127.0.0.1:8000${match[5]}`}  />
+                                                    </span>
+                                                    <span className="match-fixture__team-name"> {match[4].slice(0,3).toUpperCase()} </span>   {/*the short form for club -------------------------------------------------------*/}
+                                                </div>
+                                                <span className="icn arrow-right">
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </span>
+                                </a>
+                            </li>
+                        ))
+                    }
+                </ul>
+            </td>
+        </tr>
+    )
+}
--- frontend/src/screens/Tables.js
+++ frontend/src/screens/Tables.js
@@ -1,38 +1,158 @@
 import axios from 'axios';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { TopTableFilter } from '../components/TopTableFilter';
+import { TableMatchRow } from '../components/TableMatchRow';
 
 const Tables = () => {
-  // a local state to store the currently selected file.
-  const [selectedFile, setSelectedFile] = React.useState(null);
 
-  const handleSubmit = async(event) => {
-    event.preventDefault()
-    const formData = new FormData();
-    formData.append("selectedFile", selectedFile);
-    formData.append("selectedFile", selectedFile);
-    formData.append("selectedFile", selectedFile);
-    try {
-      const response = await axios({
-        method: "post",
-        url: "http://127.0.0.1:8000/api/user/say",
-        data: formData,
-        headers: { "Content-Type": "multipart/form-data" },
-      });
-    } catch(error) {
-      console.log(error)
+    const [week, setWeek] = useState(20)
+    const [homeAway, setHomeAway] = useState(0)
+    const [seasonIndex, setSeasonIndex] = useState(0)
+
+    const [table, setTable] = useState([])
+
+
+
+    const fetchData = async () => {
+        if (seasonIndex !== 0) {
+            try {
+                let hw = "-1"
+                if (homeAway == 1) hw = "H" 
+                else if (homeAway == 2) hw = "W"
+                const { data } = await axios.get(`http://127.0.0.1:8000/api/overview/tables?se=${seasonIndex}&mw=${week!=20?week:-1}&ha=${hw}`)
+                setTable(data)
+            } catch (error) {
+                console.log("Error fetching stats!", error)
+            }
+        }
     }
-  }
 
-  const handleFileSelect = (event) => {
-    setSelectedFile(event.target.files[0])
-  }
+    useEffect(() => {
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <input type="file" onChange={handleFileSelect}/>
-      <input type="submit" value="Upload File" />
-    </form>
-  )
+        fetchData()
+    }, [seasonIndex, week, homeAway])
+
+
+
+
+
+
+    return (
+
+        <main id="mainContent" tabindex="0">
+            <div class="tabbedContent">
+
+                <div class="league-table mainTableTab active"
+                    data-ui-tab="First Team" data-script="pl_filtered-list"
+                    data-filter-list="FIRST" data-compseason="719"
+                    data-widget="table-filtered"
+                    data-detail="2"
+                    data-live="true"
+                    data-tab-aware-default="true">
+                    <div class="wrapper">
+                        <TopTableFilter
+                            week={week}
+                            setWeek={setWeek}
+                            homeAway={homeAway}
+                            setHomeAway={setHomeAway}
+                            seasonIndex={seasonIndex}
+                            handelSeasonIndex={setSeasonIndex}
+                        />
+                    </div>
+
+                    <div class="wrapper col-12 league-table__table-header">
+                        <div class="league-table__comp-logo-container js-table-logo">
+                            <img class="league-table__comp-logo" src="https://resources.premierleague.com/premierleague/competitions/competition_1.png" />
+                        </div>
+
+                        <div class="league-table toggle-btn js-live-toggle-container is-active u-hide">
+                            <div class="live-animation"><span></span></div>
+                            <span><span class="league-table__live toggle-btn__live-text"> Live</span> League Table</span>
+                            <button class="toggle-btn__toggle js-live-toggle">
+                                <span class="toggle-btn__ball"></span>
+                            </button>
+                        </div>
+                    </div>
+
+                    <div class="league-table__all-tables-container allTablesContainer">
+                        <div class="wrapper col-12">
+
+                            <div class="tableContainer" data-round="5851">
+
+                                <div class="table league-table__table wrapper col-12">
+
+                                    <table>
+                                        <thead>
+                                            <tr>
+                                                <th class="league-table__pos-header" scope="col">
+                                                    <div class="league-table__thFull thFull">Position</div>
+                                                    <div class="league-table__thShort thShort"></div>
+                                                </th>
+                                                <th class="league-table__team-header" scope="col">Club</th>
+                                                <th scope="col"> <div class="league-table__thFull thFull">Played</div>
+                                                    <div class="league-table__thShort thShort">Pl </div>
+                                                </th>
+                                                <th scope="col">
+                                                    <div class="league-table__thFull thFull">Won </div>
+                                                    <div class="league-table__thShort thShort">W </div>
+                                                </th>
+                                                <th scope="col">
+                                                    <div class="league-table__thFull thFull">Drawn </div>
+                                                    <div class="league-table__thShort thShort">D </div>
+                                                </th>
+                                                <th scope="col">
+                                                    <div class="league-table__thFull thFull">Lost </div>
+                                                    <div class="league-table__thShort thShort">L </div>
+                                                </th>
+                                                <th class="hideSmall" scope="col">
+                                                    <abbr title="Goals For">GF </abbr>
+                                                </th>
+                                                <th class="hideSmall" scope="col">
+                                                    <abbr title="Goals Against">GA </abbr>
+                                                </th>
+                                                <th scope="col">
+                                                    <abbr title="Goal Difference">GD</abbr>
+                                                </th>
+                                                <th scope="col" class="points">
+                                                    <div class="league-table__thFull thFull">Points </div>
+                                                    <div class="league-table__thShort thShort">Pts </div>
+                                                </th> <th class="league-table__team-form hideMed" scope="col">Form</th>
+                                            </tr>
+                                        </thead>
+                                        
+
+
+                                        <tbody class="league-table__tbody isPL">
+                                            {table.length > 0 && table.map((row, index) => (
+
+                                                <TableMatchRow data={row} key={index} idx={index}/>
+                                            ))}
+                                        </tbody>
+
+
+
+
+                                    </table>
+
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
+
+
+            </div>
+
+
+        </main>
+
+
+
+
+    )
 };
 
 export default Tables
